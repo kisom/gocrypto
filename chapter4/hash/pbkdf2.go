504077c055d3bacb18f9f688d61721c411797a80
Kyle Isom <coder@kyleisom.net>
2013-04-15 21:20:38 -0600

    add pbkdf2 code to hash library

---
+++ chapter4/hash/pbkdf2.go
@@ -0,0 +1,54 @@
+package hash
+
+import (
+	_pbkdf2 "code.google.com/p/go.crypto/pbkdf2"
+	"crypto/rand"
+	"crypto/subtle"
+)
+
+var (
+        IterationCount = 16384
+        KeySize = 32
+        SaltLength = 128
+)
+
+type PasswordHash struct {
+        Salt []byte
+        Hash []byte
+}
+
+// HashPassword generates a salt and returns a hashed version of the password.
+func HashPassword(password string) *PasswordHash {
+	salt := generateSalt(SaltLength)
+	return HashPasswordWithSalt(password, salt)
+}
+
+// HashPasswordWithSalt hashes the password with the specified salt.
+func HashPasswordWithSalt(password string, salt []byte) (ph *PasswordHash) {
+	hash := _pbkdf2.Key([]byte(password), salt, IterationCount,
+                  KeySize, DefaultAlgo)
+	return &PasswordHash{hash, salt}
+}
+
+// MatchPassword compares the input password with the password hash.
+// It returns true if they match.
+func MatchPassword(password string, ph *PasswordHash) bool {
+	matched := 0
+	new_hash := HashPasswordWithSalt(password, ph.Salt)
+
+	size := len(new_hash.Hash)
+	if size > len(ph.Hash) {
+		size = len(ph.Hash)
+	}
+
+	for i := 0; i < size; i++ {
+		matched += subtle.ConstantTimeByteEq(new_hash.Hash[i], ph.Hash[i])
+	}
+
+	passed := matched == size
+	if len(new_hash.Hash) != len(ph.Hash) {
+		return false
+	}
+	return passed
+}
+
