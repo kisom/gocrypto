191b7c069f4a3cc0e293bda36b82bc2779c82802
Kyle Isom <coder@kyleisom.net>
2013-05-06 17:57:09 -0600

    throwing files in here preparing for thievery

---
+++ chapter6/timethief/filecrypt/constants.go
@@ -0,0 +1,21 @@
+package symmetric
+
+// constants for the symmetric cryptographic functions.
+
+import (
+	"crypto/aes"
+	"fmt"
+)
+
+const BlockSize = aes.BlockSize
+const KeySize = 32
+
+var Cipher = aes.NewCipher
+
+var (
+	PaddingError           = fmt.Errorf("invalid padding")
+	DegradedError          = fmt.Errorf("package is in degraded mode")
+	BadBlockError          = fmt.Errorf("bad block")
+	BlockSizeMismatchError = fmt.Errorf("block not the proper length")
+	IVSizeMismatchError    = fmt.Errorf("IV not the proper length")
+)
