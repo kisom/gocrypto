efeaaa69a38b4aab70fb70a02a479c3e1054f69d
Kyle Isom <coder@kyleisom.net>
2013-05-30 23:53:32 -0600

    Add PEM import/export to RSA-PKC.

--- chapter7/pkc/crypto.go
+++ chapter7/pkc/crypto.go
@@ -5,8 +5,10 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 const KeySize = 3072
@@ -47,12 +49,39 @@ func ExportPrivateKey(prv *rsa.PrivateKey, filename string) (err error) {
 	return
 }
 
+func ExportPrivatePEM(prv *rsa.PrivateKey, filename string) (err error) {
+	cert := x509.MarshalPKCS1PrivateKey(prv)
+	blk := new(pem.Block)
+	blk.Type = "RSA PRIVATE KEY"
+	blk.Bytes = cert
+	out, err := os.Create(filename)
+	if err == nil {
+		err = pem.Encode(out, blk)
+	}
+	return
+}
+
 func ExportPublicKey(pub *rsa.PublicKey, filename string) (err error) {
 	cert, err := x509.MarshalPKIXPublicKey(pub)
 	if err != nil {
 		return
 	}
 	err = ioutil.WriteFile(filename, cert, 0644)
+	return
+}
+
+func ExportPublicPEM(pub *rsa.PublicKey, filename string) (err error) {
+	cert, err := x509.MarshalPKIXPublicKey(pub)
+	if err != nil {
+		return
+	}
+	blk := new(pem.Block)
+	blk.Type = "RSA PUBLIC KEY"
+	blk.Bytes = cert
+	out, err := os.Create(filename)
+	if err == nil {
+		err = pem.Encode(out, blk)
+	}
 	return
 }
 
@@ -63,6 +92,9 @@ func ImportPrivateKey(filename string) (prv *rsa.PrivateKey, err error) {
 	}
 
 	prv, err = x509.ParsePKCS1PrivateKey(cert)
+	if err != nil {
+		return
+	}
 	return
 }
 
@@ -77,5 +109,40 @@ func ImportPublicKey(filename string) (pub *rsa.PublicKey, err error) {
 		return nil, err
 	}
 	pub = in.(*rsa.PublicKey)
+	return
+}
+
+func ImportPEM(filename string) (prv *rsa.PrivateKey, pub *rsa.PublicKey, err error) {
+	cert, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	for {
+		var blk *pem.Block
+		blk, cert = pem.Decode(cert)
+		if blk == nil {
+			break
+		}
+		switch blk.Type {
+		case "RSA PRIVATE KEY":
+			prv, err = x509.ParsePKCS1PrivateKey(cert)
+			if err != nil {
+				return
+			}
+		case "RSA PUBLIC KEY":
+			var in interface{}
+			in, err = x509.ParsePKIXPublicKey(cert)
+			if err != nil {
+				return
+			}
+			pub = in.(*rsa.PublicKey)
+		}
+		if cert == nil {
+			break
+		} else if pub != nil && prv != nil {
+			break
+		}
+	}
 	return
 }
