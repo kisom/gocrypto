b062af9f1b0718380766f4b5522503c703ba6557
Kyle Isom <coder@kyleisom.net>
2013-06-15 02:20:42 -0600

    Use updated DH lib.

---
+++ chapter9/dhhybrid_cek/hybrid.go
@@ -0,0 +1,31 @@
+package dhhybrid
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/gokyle/dhkam"
+	"github.com/kisom/gocrypto/chapter9/authsym"
+)
+
+var keyMaterialSize = authsym.SymKeyLen + authsym.MacKeyLen
+var ErrInvalidKEKParams = fmt.Errorf("invalid KEK parameters")
+
+func Encrypt(prv *dhkam.PrivateKey, kek *dhkam.KEK, pub *dhkam.PublicKey, m []byte) (out []byte, err error) {
+	h := sha256.New()
+	key, err := prv.CEK(kek, h)
+	if err != nil {
+		return
+	}
+	out, err = authsym.Encrypt(key[:authsym.SymKeyLen], key[authsym.SymKeyLen:], m)
+	return
+}
+
+func Decrypt(prv *dhkam.PrivateKey, kek *dhkam.KEK, pub *dhkam.PublicKey, m []byte) (out []byte, err error) {
+	h := sha256.New()
+	key, err := prv.CEK(kek, h)
+	if err != nil {
+		return
+	}
+	out, err = authsym.Decrypt(key[:authsym.SymKeyLen], key[authsym.SymKeyLen:], m)
+	return
+}
---
+++ chapter9/dhhybrid_cek/hybrid_test.go
@@ -0,0 +1,99 @@
+package dhhybrid
+
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"github.com/gokyle/dhkam"
+	"io/ioutil"
+	"testing"
+)
+
+var (
+	testSender      *dhkam.PrivateKey
+	testSenderKEK   *dhkam.KEK
+	testReceiver    *dhkam.PrivateKey
+	testReceiverKEK *dhkam.KEK
+	testct          []byte
+	testmsg         []byte
+)
+
+func TestGenerateKeys(t *testing.T) {
+	var err error
+	if testSender, err = dhkam.GenerateKey(rand.Reader); err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	if testReceiver, err = dhkam.GenerateKey(rand.Reader); err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	testSenderKEK = testSender.InitializeKEK(rand.Reader,
+	    &testReceiver.PublicKey, dhkam.AES128CBC, keyMaterialSize, nil)
+	if testSenderKEK == nil {
+		fmt.Println(ErrInvalidKEKParams.Error())
+		t.FailNow()
+	}
+
+	testReceiverKEK = testReceiver.InitializeKEK(rand.Reader,
+		&testSender.PublicKey, dhkam.AES128CBC, keyMaterialSize, nil)
+	if testReceiverKEK == nil {
+		fmt.Println(ErrInvalidKEKParams.Error())
+		t.FailNow()
+	}
+
+	if testmsg, err = ioutil.ReadFile("README"); err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+}
+
+func TestEncrypt(t *testing.T) {
+	var err error
+
+	testct, err = Encrypt(testSender, testSenderKEK, &testReceiver.PublicKey, testmsg)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	message, err := Decrypt(testReceiver, testReceiverKEK, &testSender.PublicKey, testct)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	} else if !bytes.Equal(message, testmsg) {
+		fmt.Println("dhhybrid: key exchange failure")
+		t.FailNow()
+	}
+}
+
+func BenchmarkEncryption(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ct, err := Encrypt(testSender, testSenderKEK, &testReceiver.PublicKey, testmsg)
+		if err != nil {
+			fmt.Println(err.Error())
+			b.FailNow()
+		}
+		pt, err := Decrypt(testReceiver, testReceiverKEK, &testSender.PublicKey, ct)
+		if err != nil {
+			fmt.Println(err.Error())
+			b.FailNow()
+		} else if !bytes.Equal(pt, testmsg) {
+			fmt.Println("dhhybrid: key exchange failure")
+			b.FailNow()
+		}
+	}
+}
+
+func BenchmarkKeyGeneration(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if _, err := dhkam.GenerateKey(rand.Reader); err != nil {
+			fmt.Println(err.Error())
+			b.FailNow()
+		}
+	}
+}
